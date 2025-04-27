b0d72757ac60fe99a19240b448972c688f476a77
stnvtriana <sitinovitriana03@gmail.com>
2025-04-27 19:02:57 +0700

    perubahan dari luas lingkaran saja lalu ditambah dengan volume tabung

--- src/exception/percobaan2.java
+++ src/exception/percobaan2.java
@@ -13,12 +13,21 @@ public class percobaan2 {
 int jari2 = diameter / 2; 
 double luas = Math.PI * Math.pow(jari2, 2); 
 return luas; 
-} 
+}   
+    
+  public double volumetabung(int jari_jari1, int jari_jari2, int tinggi){
+        int volume = (int) (3.14 * jari_jari1 * jari_jari2 * tinggi);
+        return volume;
+    }
 public static void main(String[] args) { 
 percobaan2 mt = new percobaan2(); 
-System.out.print(mt.luas_lingkaran(20)); 
-System.exit(0); 
-} 
+System.out.println("Luas Lingkaran : " +mt.luas_lingkaran(20)); 
+System.out.println("Volume Tabung :" +mt.volumetabung(14, 14, 24));
+System.exit(0);
+
 }
+} 
+
+
     
 
