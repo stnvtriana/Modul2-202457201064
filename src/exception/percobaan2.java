38cb10223f878301d07e8101cc0896a9718d60df
stnvtriana <sitinovitriana03@gmail.com>
2025-04-29 07:35:57 +0700

    perubahan dari in ke double di bagian tipe data diameter dan jari-jari

--- src/exception/percobaan2.java
+++ src/exception/percobaan2.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package exception;
+import java.util.Scanner;
 
 /**
  *
@@ -15,17 +16,30 @@ double luas = Math.PI * Math.pow(jari2, 2);
 return luas; 
 }   
     
-  public double volumetabung(int jari_jari1, int jari_jari2, int tinggi){
-        int volume = (int) (3.14 * jari_jari1 * jari_jari2 * tinggi);
+  public double volumetabung(double jari_jari1, double jari_jari2, int tinggi){
+        double volume =  (3.14 * jari_jari1 * jari_jari2 * tinggi);
         return volume;
     }
 public static void main(String[] args) { 
+Scanner input = new Scanner(System.in);
 percobaan2 mt = new percobaan2(); 
-System.out.println("Luas Lingkaran : " +mt.luas_lingkaran(20)); 
-System.out.println("Volume Tabung :" +mt.volumetabung(14, 14, 24));
-System.exit(0);
+System.out.println("Masukkan Diameter : "); 
+double diameter = input.nextDouble();
+System.out.println("Masukkan Jari Jari 1: ");
+double jari_jari1 = input.nextInt();
+    System.out.println("Masukkan Jari jari 2 :");
+    double jari_jari2 = input.nextInt();
+    System.out.println("Masukkan Tinggi :");
+    int tinggi = input.nextInt();
+    
+    System.out.println("Luas Lingkaran :" + mt.luas_lingkaran(20));
+    System.out.println("Volume Tabung :" + mt.volumetabung(14, 14, 21));
+
+  
 
 }
+
+
 } 
 
 
